ffdf467b84ac5ed372c2e54208b897fdd7a5506d
Empnor <eemeli.noronen@gmail.com>
2023-09-15 09:41:10 +0300

    1.8

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,65 @@
 import { useState } from 'react'    
 
-const Content = (props) => {
-  return(
-    <div>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
-      
-    </div>
-  )
+const Statistics = (props) => {
+  let hyva = props.good
+  let huono = props.bad
+  let neutraali = props.neutral
+  let all = props.kaikki
+return (
+  <div>
+    <h2>stats</h2>
+    <p>good {hyva}</p>
+    <p>neutral {neutraali}</p>
+    <p>bad {huono}</p>
+    <p>all {huono + hyva + neutraali}</p>
+    <p>{hyva / all *100 }%</p>
+    <p>{hyva / huono }</p>
+  </div>
+
+)
+
+
 }
-const Total = (props) => {
-  return <p >Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
-}
+
 
 const App = () => {
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
+  
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const kaikki = [bad + good + neutral]
 
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
 
-  const part3 = 'State of a component'
-  const exercises3 = 14
 
+  
   return (
-    <div>
-      <Header />
-      <Content 
-      part1={part1} 
-      part2={part2} 
-      part3={part3}
-      exercises1={exercises1} 
-      exercises2={exercises2} 
-      exercises3={exercises3} 
-      />
-      <Total
-      exercises1={exercises1} 
-      exercises2={exercises2} 
-      exercises3={exercises3}  
-      />
-    </div>
-  )
+
+    
+    <><h1>give feedback</h1><div>
+      <div>
+        <button onClick={() => setGood(good + 1)}>
+          good
+
+        </button>
+        <button onClick={() => setNeutral(neutral + 1)}>
+          neutral
+
+        </button>
+
+        <button onClick={() => setBad(bad + 1)}>
+          bad
+        </button>
+
+        <Statistics 
+          good={good}
+          bad={bad}
+          neutral={neutral}
+          kaikki={kaikki}
+        />
+
+      </div>
+    </div></>
+   )
 }
 
 export default App
